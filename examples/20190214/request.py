9676906912c96aa3e56d82efbbf4f7ab6b07afc1
Hannu Oksman <hannu@muaddibs.net>
2019-02-21 12:07:43 +0200

    http example done

--- examples/20190214/request.py
+++ examples/20190214/request.py
@@ -38,16 +38,83 @@ class HttpRequest:
         print "Request: request's write method"
         # https://tools.ietf.org/html/rfc7230#section-3.1.1
         f.write("%s %s HTTP/1.1\r\n" % (self.method,self.resource))
-        self.write_header(f,self.headers)
+        write_header(self,f,self.headers)
         f.write("\r\n")
         f.flush() #tyhjentää bufferin eli pakoittaa kirjoituksen file-objektiin ilman sen sulkemista
         print "Request sent"
 
-    def write_header(self,f,headers):
-        print "write_headers"
-        # https://tools.ietf.org/html/rfc7230#section-3.2
-        for header,value in headers.iteritems():
-            f.write("%s: %s\r\n" % (header,value))
+def write_header(self,f,headers):
+#    print "write_headers"
+    # https://tools.ietf.org/html/rfc7230#section-3.2
+    for header,value in headers.iteritems():
+        f.write("%s: %s\r\n" % (header,value))
+
+# luodaan uusi luokka demonstroimaan http-responea
+# tärkeää huomata, että statusline eroaa requestlinesta
+class HttpResponse:
+    def __init__(self,status_code,reason,body,headers={}):
+        self.status_code = status_code
+        self.reason = reason
+        self.body = body
+        self.headers = headers
+        # https://tools.ietf.org/html/rfc7230#section-3
+
+    @staticmethod
+    def read_from(f):
+        status_code,reason = HttpResponse.read_statusline(f)
+        headers = read_headers(f)
+        body = "".join(f.readlines())
+        return HttpResponse(status_code,reason,body,headers)
+
+    def write_to(self,f):
+        f.write("HTTP/1.1 %d %s\r\n" % (self.status_code,self.reason))
+        write_header(self,f,self.headers)
+        f.write("\r\n")
+        f.write("%s" % self.body)
+        f.flush()
+
+    # luodaanstaattinen metodi statuslinen lukemiseen
+    # annetaan parametrina fileobject
+    # staattista metodia kutsutaan luokan nimellä ja metodilla
+    @staticmethod
+    def read_statusline(f):
+        # readline metodi palauttaa string kaikki ennen \n merkkiä ja myös sen
+        # siistitään merkkijono strip(), joka poistaa whitespaces
+        status_line = f.readline().strip()
+
+        # number of items, matches rfc?
+        # reason may have multiple items when split with sp
+        parts = status_line.split(" ")
+        print len(parts)
+        if len(parts) < 3:
+            print "MalformedStatusline"
+
+        version = parts[0]
+        if version != "HTTP/1.1":
+            print "UnsupportedHttpVersion"
+
+        try:
+            status_code = int(parts[1])
+        except ValueError:
+            print "Not a number"
+
+        reason = " ".join(parts[2:]).strip()
+
+        return (status_code,reason)
+
+    def read_headers(self,f):
+        headers = {}
+        for rawline in f:
+            line = rawline.strip()
+            if line == "":
+                break
+            parts = line.split(":")
+            if len(parts) < 2:
+                print "MalformedHttpHeader"
+            key = parts[0].strip()
+            value = ".".join.parts[1:].strip()
+            headers[key] = value
+        return headers
 
 class UnknownHTTPMethodException(Exception):
     pass
--- examples/20190214/test.py
+++ examples/20190214/test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from request import HttpRequest
+from request import HttpRequest,HttpResponse
 import socket
+import sys
 
 def main():
     host = "httpbin.org"
@@ -17,4 +18,28 @@ def main():
     f.close()
     sock.close()
 
-main()
+def main1():
+    host = "httpbin.org"
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.connect((host,80))
+
+    # https://tools.ietf.org/html/rfc7230#section-6.1
+    req = HttpRequest("GET","/",headers={"Host":host,"Connection":"close"})
+
+    f = sock.makefile()
+    req.write_to(f)
+
+    '''
+    response = HttpResponse.read_from(f)
+    print response.status_code
+    print "tästä alkaa responsen body"
+    print response.body
+    '''
+
+    response = HttpResponse(200,"OK","Olen torso\n",headers={"Host":"localhost"})
+    response.write_to(sys.stdout)
+
+    f.close()
+    sock.close()
+
+main1()
